b0bf06e366be0f1427485863a41b8d651b4665d5
andres.sarmiento@assis.com.co <andres.sarmiento@assis.com.co>
2022-05-11 10:55:11 -0500

    Se realizan modificaciones sobre el modelo de Auditoria Procesos

--- app/Models/Seguridad/AuditoriaProceso.php
+++ app/Models/Seguridad/AuditoriaProceso.php
@@ -32,20 +32,23 @@ class AuditoriaProceso extends Model
         if(isset($dto['nombre_recurso'])){
             $query->where('audProTransaccion', 'like', "%" . $dto['nombre_recurso'] . "%");
         }
+        if(isset($dto['numero_proyecto'])){
+            $query->where('audProNumeroProyecto', 'like', "%" . $dto['numero_proyecto'] . "%");
+        }
         if(isset($dto['nombre_responsable'])){
             $query->where('audProUsuarioCreacionNombre', 'like', "%" . $dto['nombre_responsable'] . "%");
         }
+        if(isset($dto['tipo'])){
+            $query->where('audProTipo', 'like', "%" . $dto['tipo'] . "%");
+        }
         if(isset($dto['descripcion_recurso'])){
-            $query->where('audProDescripcion', 'like', '%' . $dto['audProDescripcion'] . '%');
+            $query->where('audProDescripcion', 'like', '%' . $dto['descripcion_recurso'] . '%');
         }
         if(isset($dto['fecha_desde'])){
             $query->where('created_at', '>=', $dto['fecha_desde'] . ' 00:00:00');
         }
         if(isset($dto['fecha_hasta'])){
             $query->where('created_at', '<=', $dto['fecha_hasta'] . ' 23:59:59');
-        }
-        if(isset($dto['accion'])){
-            $query->where('accion', 'like', "%" . $dto['accion'] . "%");
         }
         if (isset($dto['ordenar_por']) && count($dto['ordenar_por']) > 0){
             foreach ($dto['ordenar_por'] as $attribute => $value){
@@ -58,14 +61,14 @@ class AuditoriaProceso extends Model
                 if($attribute == 'audProDescripcion'){
                     $query->orderBy('audProDescripcion', $value);
                 }
+                if($attribute == 'audProTipo'){
+                    $query->orderBy('audProTipo', $value);
+                }
                 if($attribute == 'audProUsuarioCreacionNombre'){
                     $query->orderBy('audProUsuarioCreacionNombre', $value);
                 }
-                if($attribute == 'accion'){
-                    $query->orderBy('accion', $value);
-                }
-                if($attribute == 'responsable_nombre'){
-                    $query->orderBy('responsable_nombre', $value);
+                if($attribute == 'audProNumeroProyecto'){
+                    $query->orderBy('audProNumeroProyecto', $value);
                 }
                 if($attribute == 'fecha'){
                     $query->orderBy('created_at', $value);
